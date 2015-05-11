1c96af37e0e63ca74de111e033b97d62bdd6308e
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-05-11 16:51:56 +0530

    finished gsl wrapper

--- lib/daru/accessors/gsl_wrapper.rb
+++ lib/daru/accessors/gsl_wrapper.rb
@@ -7,11 +7,16 @@ module Daru
       extend Forwardable
 
       def_delegators :@data, :[], :[]=, :size, :to_a, :each, :mean, 
-        :sum, :prod, :max, :min, :map!
+        :sum, :prod, :max, :min
 
       alias :product :prod
 
       attr_reader :data
+
+      def map!(&block)
+        @data.map!(&block)
+        self
+      end
 
       def initialize data, context
         @data = ::GSL::Vector.alloc(data)
@@ -19,21 +24,26 @@ module Daru
       end
 
       def delete_at index
-        
+        @data.delete_at index
       end
 
       def index key
-        
+        @data.to_a.index key
       end
 
       def push value
-        
+        @data = @data.concat value
+        self
       end
-
       alias :<< :push
+      alias :concat :push
 
       def dup
         
+      end
+
+      def == other
+        @data == other.data
       end
     end
   end
