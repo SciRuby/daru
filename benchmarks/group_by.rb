c97af2f7a44fa73d8a6c1fac72e7ea9ead5cbb20
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-07-25 01:07:40 +0530

    added group by benchmark

---
+++ benchmarks/group_by.rb
@@ -0,0 +1,32 @@
+$:.unshift File.expand_path("../../lib", __FILE__)
+
+require 'benchmark'
+require 'daru'
+
+data = Daru::DataFrame.from_csv 'TradeoffData.csv'
+
+Benchmark.bm do |x|
+  x.report("Single column grouping") do
+    @single = data.group_by([:Treatment])
+  end
+
+  x.report("Multi-column grouping") do
+    @multi = data.group_by([:Group, :Treatment])
+  end
+
+  x.report("Single mean") do
+    @single.mean
+  end
+
+  x.report("Multi mean") do
+    @multi.mean
+  end
+end
+
+#                    ===== Benchmarks =====
+# 
+#                          user     system      total        real
+# Single column grouping 0.000000   0.000000   0.000000   (0.000356)
+# Multi-column grouping  0.000000   0.000000   0.000000   (0.000958)
+# Single mean            0.000000   0.000000   0.000000   (0.000865)
+# Multi mean             0.000000   0.000000   0.000000   (0.002748)
