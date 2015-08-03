9462b93456837dccab872c87eaa202cf330048c2
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-08-03 09:42:16 +0530

    Added filter vs where benchmarks (where is much faster) and updated gemspec and rakefile

--- Rakefile
+++ Rakefile
@@ -1,6 +1,21 @@
 require 'rspec/core/rake_task'
 require 'bundler/gem_tasks'
 
+lib_folder = File.expand_path("../lib", __FILE__)
 RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
+
+task :console do |task|
+  cmd = [ 'irb', "-r '#{lib_folder}/daru.rb'" ]
+  run *cmd
+end
+
+task :pry do |task|
+  cmd = [ 'pry', "-r '#{lib_folder}/daru.rb'" ]
+  run *cmd
+end
+
+def run *cmd
+  sh(cmd.join(" "))
+end
---
+++ benchmarks/where_vs_filter.rb
@@ -0,0 +1,28 @@
+$:.unshift File.expand_path("../../lib", __FILE__)
+
+require 'benchmark'
+require 'daru'
+
+df = Daru::DataFrame.new({
+  a: [1,2,3,4,5,6]*100,
+  b: ['a','b','c','d','e','f']*100,
+  c: [11,22,33,44,55,66]*100
+}, index: (1..600).to_a.shuffle)
+
+Benchmark.bm do |x|
+  x.report("where") do
+    df.where(df[:a].eq(2) | df[:c].eq(55))
+  end
+
+  x.report("filter_rows") do
+    df.filter(:row) do |r|
+      r[:a] == 2 or r[:c] == 55
+    end
+  end
+end
+
+#             ===== Benchmarks =====
+# 
+#                user     system      total        real
+# where        0.000000   0.000000   0.000000 (  0.002575)
+# filter_rows  0.210000   0.000000   0.210000 (  0.205403)
