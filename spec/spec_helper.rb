467f889745f3dc6ff84133b6160ae86c5180a543
zverok <zverok.offline@gmail.com>
2016-05-18 20:16:40 +0300

    Enforce Index coverage (and change inspect)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,9 @@ end
 RSpec::Expectations.configuration.warn_about_potential_false_positives = false
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+ add_filter 'spec'
+end
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
