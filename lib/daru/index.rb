5698bc60ba0676a73579972f784c89cfeff90482
lokeshh <lokeshhsharma@gmail.com>
2016-05-28 19:11:40 +0000

    update comment for each

--- lib/daru/index.rb
+++ lib/daru/index.rb
@@ -488,9 +488,9 @@ module Daru
     
     def each
       return @array.each
-      # Solve this error
-      return enum_for(:each) unless block_given?      
-      @array.map { |pos| yield @map_int_cat[pos] }
+      # Its causing error do not know why
+      # return enum_for(:each) unless block_given?      
+      # @array.map { |pos| yield @map_int_cat[pos] }
     end
 
     def to_a
