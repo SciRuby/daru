4d1f4f0a4c95eceb4087688c0ac08edea257ba6e
lokeshh <lokeshhsharma@gmail.com>
2016-06-06 20:49:33 +0000

    create categorical code and spec files

---
+++ spec/categorical_spec.rb
@@ -0,0 +1,37 @@
+describe Daru::Vector do
+  context "initialize" do
+    let(:dv) { Daru::Vector.new [:a, 1, :a, 1, :c], type: :category }
+    subject { dv }
+    
+    it { is_expected.to be_a Daru::Vector }
+    its(:size) { is_expected.to eq 5 }
+    its(:type) { is_expected.to eq :category }
+    its(:ordered?) { is_expected.to eq false }
+    its(:to_a) { is_expected.to eq [:a, 1, :a, 1, :c] }
+  end
+  
+  context "#to_factor!" do
+    let(:dv) { Daru::Vector.new [:a, 1, :a, 1, :c] }
+    subject { dv }
+    before { dv.to_factor! }
+    
+    it { is_expected.to be_a Daru::Vector }
+    its(:size) { is_expected.to eq 5 }
+    its(:type) { is_expected.to eq :category }
+    its(:ordered?) { is_expected.to eq false }
+    its(:to_a) { is_expected.to eq [:a, 1, :a, 1, :c] }
+  end
+  
+  context "#category" do
+    let(:dv) { Daru::Vector.new [:a, 1, :a, 1, :c], type: :category }
+    subject { dv.category }
+    
+    it { is_expected.to be_a Array }
+    its(:size) { is_expected.to eq 3 }
+    its(:'to_a') { is_expected.to eq [:a, 1, :c] }
+  end
+  
+  context "#[]" do
+
+  end
+end
