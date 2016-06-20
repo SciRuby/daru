411b6fe4b65b6a7f422cebc4bdbae81491cdd7ca
lokeshh <lokeshhsharma@gmail.com>
2016-06-20 19:35:29 +0000

    add support for dataframe categorized by categories

--- lib/daru/plotting/dataframe.rb
+++ lib/daru/plotting/dataframe.rb
@@ -19,7 +19,14 @@ module Daru
       #   # Simple bar chart
       #   df = Daru::DataFrame.new({a:['A', 'B', 'C', 'D', 'E'], b:[10,20,30,40,50]})
       #   df.plot type: :bar, x: :a, y: :b
-      def plot opts={}
+      def plot opts={}, &block
+        opts[:category]? plot_with_category(opts, &block) :
+          plot_without_category(opts, &block)
+      end
+
+      private
+
+      def plot_without_category opts
         options = {type:  :scatter}.merge(opts)
 
         plot = Nyaplot::Plot.new
@@ -40,7 +47,54 @@ module Daru
         plot.show
       end
 
-      private
+      def plot_with_category opts
+        plot = Nyaplot::Plot.new
+        category_opts = opts[:category]
+        type = opts[:type]
+        case type
+        when :line, :scatter
+          x, y = opts[:x], opts[:y]
+          cat_dv = self[category_opts[:by]]
+
+          diagrams = cat_dv.categories.map do |cat|
+            x_vec = self[x].where(cat_dv.eq cat)
+            y_vec = self[y].where(cat_dv.eq cat)
+            df = Daru::DataFrame.new [x_vec, y_vec], order: [x, y]
+            nyaplot_df = df.to_nyaplotdf
+    
+            plot.add_with_df(nyaplot_df, type, x, y)
+          end
+        else
+          raise ArgumentError, "Unsupported type #{type}"
+        end
+
+        method = category_opts[:method]
+        colors = get_color
+        diagrams.zip cat_dv.categories do |d, cat|
+          d.title cat
+          case method
+          when :color
+            d.color colors.next
+          when :shape
+            # TODO: Add categorization by shape
+          when :size
+            # TODO: Add categorization by size
+          else
+            raise ArgumentError, "Unkown supported method #{method}"
+          end
+        end
+
+        plot.legend true
+        yield plot, *diagrams if block_given?
+        plot.show
+      end
+
+      def get_color
+        return to_enum(:get_color) unless block_given?        
+        loop do
+          Nyaplot::Colors.qual.each { |col| yield col }
+        end
+      end
 
       def single_diagram? options
         options[:x] && options[:x].is_a?(Symbol)
