b33aef20fcd2551f8c11de3c15766b0a2434e02a
lokeshh <lokeshhsharma@gmail.com>
2016-06-24 02:46:04 +0000

    start write plotting rspec

--- spec/plotting/vector_spec.rb
+++ spec/plotting/vector_spec.rb
@@ -63,3 +63,30 @@ describe Daru::Vector, 'plotting' do
     end
   end
 end
+
+describe Daru::Vector, 'plotting category' do
+  let(:plot) { instance_double('Nyaplot::Plot') }
+  let(:diagram) { instance_double('Nyaplot::Diagram') }  
+  before do
+    allow(Nyaplot::Plot).to receive(:new).and_return(plot)
+  end
+  context 'bar' do
+    let(:dv) do
+      Daru::Vector.new ['III']*10 + ['II']*5 + ['I']*5,
+        type: :category,
+        categories: ['I', 'II', 'III']
+    end
+    subject do 
+      dv.plot(type: :bar) do |p, d|
+        p.x_label 'Categories'
+        p.y_label 'Frequency'
+      end
+    end
+      
+    it do
+      expect(plot).to receive(:add).with(:bar, ['I', 'II', 'III'], [5, 5, 10])
+      expect(plot).to receive(:show)
+      dv.plot(type: :bar)
+    end
+  end
+end
