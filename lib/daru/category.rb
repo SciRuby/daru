2e7bef10f08de16aeec37db573462b6530428a6d
lokeshh <lokeshhsharma@gmail.com>
2016-08-14 23:58:37 +0000

    improve code

--- lib/daru/category.rb
+++ lib/daru/category.rb
@@ -674,8 +674,7 @@ module Daru
     end
 
     def replace old_values, new_value
-      old_values = [old_values] unless old_values.is_a? Array
-      rename_hash = old_values.map { |v| [v, new_value] }.to_h
+      rename_hash = [*old_values].map { |v| [v, new_value] }.to_h
       rename_categories rename_hash
     end
 
