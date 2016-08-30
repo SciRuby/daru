be401b88becf8de950dd57a4fe95983fe54d25c4
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2016-08-30 23:39:00 +0530

    Add extra else condition Category Vector#frequencies. Assign method to :count in gruff plotting if method is not specified.

--- lib/daru/category.rb
+++ lib/daru/category.rb
@@ -68,7 +68,7 @@ module Daru
           "Daru::Plotting::Category::#{lib.to_s.capitalize}Library"
         ) if Daru.send("has_#{lib}?".to_sym)
       else
-        raise ArguementError, "Plotting library #{lib} not supported. "\
+        raise ArgumentError, "Plotting library #{lib} not supported. "\
           'Supported libraries are :nyaplot and :gruff'
       end
     end
@@ -160,6 +160,9 @@ module Daru
           counts.map { |c| c / size.to_f }
         when :percentage
           counts.map { |c| c / size.to_f * 100 }
+        else
+          raise ArgumentError, 'Type should be either :count, :fraction or'\
+          " :percentage. #{type} not supported."
         end
       Daru::Vector.new values, index: categories, name: name
     end
