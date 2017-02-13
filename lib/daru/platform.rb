0fd6b7f58979b58ae941a8c0dacd4c84e22ef635
Rick Hull <rick.hull@gmail.com>
2017-02-13 00:06:21 +0000

    move date_time constants to date_time/offsets, the only user

--- lib/daru/platform.rb
+++ lib/daru/platform.rb
@@ -5,31 +5,6 @@ end
 # :nocov:
 
 module Daru
-  DAYS_OF_WEEK = {
-    'SUN' => 0,
-    'MON' => 1,
-    'TUE' => 2,
-    'WED' => 3,
-    'THU' => 4,
-    'FRI' => 5,
-    'SAT' => 6
-  }.freeze
-
-  MONTH_DAYS = {
-    1 => 31,
-    2 => 28,
-    3 => 31,
-    4 => 30,
-    5 => 31,
-    6 => 30,
-    7 => 31,
-    8 => 31,
-    9 => 30,
-    10 => 31,
-    11 => 30,
-    12 => 31
-  }.freeze
-
   MISSING_VALUES = [nil, Float::NAN].freeze
 
   @lazy_update = false
