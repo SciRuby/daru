42ec1ae1262cb8176940c6a1ff1aba8b6fa9e666
Siva Gollapalli <sivagollapalli88@gmail.com>
2017-05-25 16:01:19 +0530

    Using error stream for printing

--- lib/daru.rb
+++ lib/daru.rb
@@ -38,11 +38,13 @@ module Daru
 
   @plotting_library = :nyaplot
 
+  @@error_stream = $stderr
+
   class << self
     # A variable which will set whether Vector metadata is updated immediately or lazily.
     # Call the #update method every time a values are set or removed in order to update
     # metadata like positions of missing values.
-    attr_accessor :lazy_update, :error_stream
+    attr_accessor :lazy_update
     attr_reader :plotting_library
 
     def create_has_library(library)
@@ -72,6 +74,14 @@ module Daru
         raise ArgumentError, "Unsupported library #{lib}"
       end
     end
+
+    def error_stream= stream 
+      class_variable_set(:@@error_stream, stream)
+    end
+
+    def error_stream
+      @@error_stream
+    end
   end
 
   create_has_library :gsl
@@ -84,8 +94,8 @@ begin
   gem 'spreadsheet', '~>1.1.1'
   require 'spreadsheet'
 rescue LoadError
-  STDERR.puts "\nInstall the spreadsheet gem version ~>1.1.1 for using"\
-  ' spreadsheet functions.'
+  Daru.error_stream.puts "\nInstall the spreadsheet gem version ~>1.1.1 for using"\
+  ' spreadsheet functions.' if Daru.error_stream
 end
 
 autoload :CSV, 'csv'
--- spec/daru_spec.rb
+++ spec/daru_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe 'Daru' do
-  it 'able to set error stream' do
-    Daru.error_stream = STDERR
-    expect(Daru.error_stream).to eq(STDERR)
+  it 'default error stream should be $stderr' do
+    expect(Daru.error_stream).to eq($stderr)
+  end
+
+  it 'should be able to set error stream to nil' do
     Daru.error_stream = nil
-    expect(Daru.error_stream).to eq(nil)
+    expect(Daru.error_stream).to be_nil
   end
 end
