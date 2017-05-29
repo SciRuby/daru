b17ace3147a57e063ab2013344f32ac150067fff
Siva Gollapalli <sivagollapalli88@gmail.com>
2017-05-29 12:43:36 +0530

    [#242] Migrating error stream into a method

--- spec/daru_spec.rb
+++ spec/daru_spec.rb
@@ -9,4 +9,18 @@ describe 'Daru' do
     Daru.error_stream = nil
     expect(Daru.error_stream).to be_nil
   end
+
+  it 'should print error message if error stream set to $stderr' do
+    Daru.error_stream = $stderr
+    allow($stderr).to receive(:puts).and_return(true)
+    Daru.error('priting error message')
+    expect($stderr).to have_received(:puts)
+  end
+
+  it 'should not print error message if it is set to nil' do
+    Daru.error_stream = nil
+    allow($stderr).to receive(:puts).and_return(true)
+    Daru.error('priting error message')
+    expect($stderr).not_to have_received(:puts)
+  end
 end
