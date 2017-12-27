20bb6f73f4ff6b6c8f1dc67a9b571bf98d277260
Mitsutaka Mimura <takkanm@gmail.com>
2017-12-27 19:11:33 +0900

    allow symbol to converters option (#445)
    CSV.new is allow only symbol converters option.

--- spec/io/io_spec.rb
+++ spec/io/io_spec.rb
@@ -51,6 +51,11 @@ describe Daru::IO do
         expect(df['Domestic'].to_a).to all be_boolean
       end
 
+      it "allow symbol to converters option" do
+        df = Daru::DataFrame.from_csv 'spec/fixtures/boolean_converter_test.csv', converters: :boolean
+        expect(df['Domestic'].to_a).to all be_boolean
+      end
+
       it "checks for equal parsing of local CSV files and remote CSV files" do
         %w[matrix_test repeated_fields scientific_notation sales-funnel].each do |file|
           df_local  = Daru::DataFrame.from_csv("spec/fixtures/#{file}.csv")
