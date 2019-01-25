ec20477df56249bc71a2db36dc11f627f9854090
shekharrajak <shekharrajak@live.com>
2019-01-25 19:04:14 +0530

    Daru::DataFrame benchamrk - various ways of initializing dataframe

---
+++ benchmarks/statistics/benchmark_dataframe_creation.rb
@@ -0,0 +1,98 @@
+$:.unshift File.expand_path("../../../lib", __FILE__)
+
+require_relative './dataframe/dataframeCreationBenchmark'
+
+module DaruBenchmark
+  class DataFrameBenchmark
+    extend DaruBenchmark::DataFrame
+    DF_SIZE_POW_10 = [2, 3, 4, 5]
+
+    def self.benchmark_daru_dataframe()
+      DF_SIZE_POW_10.each do |df_size|
+        puts "DataFrame of size : 10 ** #{df_size} "
+        self.set_df_row_size(10**df_size)
+        self.init()
+        self.benchmark_dataframe_creation()
+        puts 
+      end
+    end
+  end # end DataFrameBenchmark class
+end # end DaruBenchmark
+
+DaruBenchmark::DataFrameBenchmark.benchmark_daru_dataframe()
+
+# Jan 2019
+# DataFrame of size : 10 ** 2 
+# Warming up --------------------------------------
+#  Using list of lists   118.000  i/100ms
+# Using list of Vector    37.000  i/100ms
+#  Using Hash of lists   855.000  i/100ms
+# Using Hash of Vector   347.000  i/100ms
+# Calculating -------------------------------------
+#  Using list of lists      1.228k (± 2.7%) i/s -      6.136k in   5.000674s
+# Using list of Vector    379.640  (± 2.6%) i/s -      1.924k in   5.071989s
+#  Using Hash of lists      8.697k (± 3.5%) i/s -     43.605k in   5.020186s
+# Using Hash of Vector      3.502k (± 3.8%) i/s -     17.697k in   5.061505s
+
+# Comparison:
+#  Using Hash of lists:     8697.3 i/s
+# Using Hash of Vector:     3501.7 i/s - 2.48x  slower
+#  Using list of lists:     1228.0 i/s - 7.08x  slower
+# Using list of Vector:      379.6 i/s - 22.91x  slower
+
+
+# DataFrame of size : 10 ** 3 
+# Warming up --------------------------------------
+#  Using list of lists    13.000  i/100ms
+# Using list of Vector     3.000  i/100ms
+#  Using Hash of lists   215.000  i/100ms
+# Using Hash of Vector    65.000  i/100ms
+# Calculating -------------------------------------
+#  Using list of lists    129.814  (± 3.1%) i/s -    650.000  in   5.011517s
+# Using list of Vector     38.721  (± 2.6%) i/s -    195.000  in   5.040975s
+#  Using Hash of lists      2.125k (± 3.6%) i/s -     10.750k in   5.066062s
+# Using Hash of Vector    664.555  (± 3.8%) i/s -      3.380k in   5.093825s
+
+# Comparison:
+#  Using Hash of lists:     2124.8 i/s
+# Using Hash of Vector:      664.6 i/s - 3.20x  slower
+#  Using list of lists:      129.8 i/s - 16.37x  slower
+# Using list of Vector:       38.7 i/s - 54.88x  slower
+
+
+# DataFrame of size : 10 ** 4 
+# Warming up --------------------------------------
+#  Using list of lists     1.000  i/100ms
+# Using list of Vector     1.000  i/100ms
+#  Using Hash of lists    24.000  i/100ms
+# Using Hash of Vector     7.000  i/100ms
+# Calculating -------------------------------------
+#  Using list of lists     13.151  (± 7.6%) i/s -     66.000  in   5.036990s
+# Using list of Vector      3.311  (± 0.0%) i/s -     17.000  in   5.216135s
+#  Using Hash of lists    243.654  (± 6.2%) i/s -      1.224k in   5.047603s
+# Using Hash of Vector     71.068  (± 2.8%) i/s -    357.000  in   5.028227s
+
+# Comparison:
+#  Using Hash of lists:      243.7 i/s
+# Using Hash of Vector:       71.1 i/s - 3.43x  slower
+#  Using list of lists:       13.2 i/s - 18.53x  slower
+# Using list of Vector:        3.3 i/s - 73.58x  slower
+
+
+# DataFrame of size : 10 ** 5 
+# Warming up --------------------------------------
+#  Using list of lists     1.000  i/100ms
+# Using list of Vector     1.000  i/100ms
+#  Using Hash of lists     1.000  i/100ms
+# Using Hash of Vector     1.000  i/100ms
+# Calculating -------------------------------------
+#  Using list of lists      1.317  (± 0.0%) i/s -      7.000  in   5.342037s
+# Using list of Vector      0.349  (± 0.0%) i/s -      2.000  in   5.737458s
+#  Using Hash of lists     20.781  (±19.2%) i/s -     96.000  in   5.012213s
+# Using Hash of Vector      6.310  (±15.8%) i/s -     32.000  in   5.159075s
+
+# Comparison:
+#  Using Hash of lists:       20.8 i/s
+# Using Hash of Vector:        6.3 i/s - 3.29x  slower
+#  Using list of lists:        1.3 i/s - 15.78x  slower
+# Using list of Vector:        0.3 i/s - 59.52x  slower
--- benchmarks/statistics/dataframe/dataframeCreationBenchmark.rb
+++ benchmarks/statistics/dataframe/dataframeCreationBenchmark.rb
@@ -1,39 +1,146 @@
-module DataFrameBenchmark
-  def benchmark_create_df_listOflists(size)
-    bench = Benchmark.bm do |x|
-      report = x.report('Create DataFrame of size :' + size.to_s + ' : ') do
-        self.generate_df(size)
+$:.unshift File.expand_path("../../../../lib", __FILE__)
+
+require 'daru'
+require 'benchmark/ips'
+
+module DaruBenchmark
+  # TDOD exception handling
+  module DataFrame
+    @@df = nil
+    @@df_row_size = nil
+    @@rows = nil
+    @@rows_vectors = nil
+    @@hash_list = nil
+    @@hash_vector = nil
+    @@df_colmn_size = 2
+
+    def set(set_df)
+      @@df = set_df
+    end
+
+    def get
+      @@df
+    end
+
+    def set_df_row_size(set_size)
+      @@df_row_size = set_size
+    end
+
+    def get_df_row_size
+      @@df_row_size
+    end
+
+    def init()
+      create_rows()
+      rows_to_vectors()
+      create_hash_with_list()
+      create_hash_with_vector()
+    end
+
+    def benchmark_dataframe_creation
+      Benchmark.ips do |x|
+        # Configure the number of seconds used during
+        # the warmup phase (default 2) and calculation phase (default 5)
+        x.config(:time => 5, :warmup => 2)
+
+        # These parameters can also be configured this way
+        x.time = 5
+        x.warmup = 2
+
+        # Typical mode, runs the block as many times as it can
+        x.report("Using list of lists") do 
+          create_df_listOfLists()
+        end
+
+        x.report("Using list of Vector") do
+          create_df_listOfVector()
+        end
+
+        x.report("Using Hash of lists") do
+          create_df_using_hash_with_list()
+        end
+
+        x.report("Using Hash of Vector") do
+          create_df_using_hash_with_vector()
+        end
+
+        # Compare the iterations per second of the various reports!
+        x.compare!
       end
-      @result_create_df.append("%1.20f" % report.real)
+    end
+
+    # Methods to create DF with different data format
+    def create_df_listOfLists()
+      # for size * size dataframe
+      # @df= Daru::DataFrame.new(
+      #   Array.new(size) { Array.new(size) { size*rand(1..9) }  }
+      # )
+
+      # creaating dataframe of size =  size * 2
+      @@df= Daru::DataFrame.new(
+        @@rows
+      )
+      # p 'Size of the dataframe: (row x coln) : ' + @@df.shape.to_s 
+    end
+
+    # with index option
+    def create_df_listOfLists_with_index()
+      # pass
+    end
+
+    def create_df_listOfVector()
+      # creaating dataframe of size =  size * 2
+      @@df= Daru::DataFrame.new(
+        @@rows_vectors
+      )
+    end
+
+    # TODO: exception handling if @@hash_list is nil
+    def create_df_using_hash_with_list()
+      # creaating dataframe of size =  size * 2
+      @@df= Daru::DataFrame.new(
+        @@hash_list
+      )
+    end
+
+    def create_df_using_hash_with_vector()
+      # creaating dataframe of size =  size * 2
+      @@df= Daru::DataFrame.new(
+        @@hash_vector
+      )
+    end
+
+
+    # Helping methods
+    def create_rows()
+      @@rows = Array.new(@@df_row_size) { Array.new(2) { rand(1..9) } }
+    end
+
+    def rows_to_vectors()
+      @@rows_vectors = @@rows.map { |r| Daru::Vector.new r }
+    end
+
+    def rows_to_vectors_with_index()
+      @@rows_vectors = @@rows.map { |r| Daru::Vector.new r, index: [:a,:b] }
+    end
+
+    def generate_symbols_for_colmn_name(size)
+      (10..(10 + size)).map{|i| (i.to_s 36).to_sym}
+    end
+
+    def create_hash_with_list()
+      @@hash_list = Hash.new []
+      generate_symbols_for_colmn_name(@@df_colmn_size).each do |colmn|  
+        @@hash_list[colmn] = Array.new(@@df_row_size) { rand(1..9) }
+      end
+    end
+
+    def create_hash_with_vector()
+      @@hash_vector = Hash.new []
+      generate_symbols_for_colmn_name(@@df_colmn_size).each do |colmn|  
+        @@hash_vector[colmn] = Daru::Vector.new(Array.new(@@df_row_size) { rand(1..9) })
+      end
     end
   end
-
-  def create_df_listOfLists(size)
-    # for size * size dataframe
-    # @df= Daru::DataFrame.new(
-    #   Array.new(size) { Array.new(size) { size*rand(1..9) }  }
-    # )
-
-    # creaating dataframe of size =  size * 2
-    @df= Daru::DataFrame.new(
-      Array.new(size) { Array.new(2) { 2*rand(1..9) }  }
-    )
-    @df_size = size
-  end
-
-  def create_df_listOfVector(size)
-    # pass
-  end
-
-  def create_df_listOfHashes(size)
-    # pass
-  end
-
-  def create_df_hashOfLists(size)
-    # pass
-  end
-
-  def create_df_hashOfVector(size)
-    # pass
-  end
+  # TODO: for DataFrame having MultiIndex
 end
