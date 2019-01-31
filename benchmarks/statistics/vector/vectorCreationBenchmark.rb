130c13c0c6a684275d439a27ddc6173957d739ca
shekharrajak <shekharrajak@live.com>
2019-01-31 16:59:05 +0530

    removing files after refactoring the code

--- benchmarks/statistics/vector/vectorCreationBenchmark.rb
+++ benchmarks/statistics/vector/vectorCreationBenchmark.rb
@@ -7,19 +7,19 @@ require 'daru'
 module DaruBenchmark
   module Vector
     def vectorCreation_array
-
+        #TODO
     end
 
     def vectorCreation_nmatrixArray
-
+        #TODO
     end
 
     def vectorCreation_gsl
-
+        #TODO
     end
 
     def vectorCreation_mdarray
-
+        #TODO
     end
   end
 end
